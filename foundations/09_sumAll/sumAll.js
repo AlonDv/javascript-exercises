bb0ccc4fb4d0557e24d44d391c7a5f9ee88ddf4a
Alon <217150097+AlonDv@users.noreply.github.com>
2026-03-25 16:31:32 -0700

    Complete 09_SUMALL exercise.

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(input1, input2) {
+    let bigNum = (input1 > input2) ? input1 : input2;
+    let smallNum = (input1 < input2) ? input1 : input2;
+    let sum = 0; 
 
+    if(Number.isInteger(input1) && Number.isInteger(input2) && bigNum > 0 && smallNum > 0){
+        for(let i = smallNum; i<=bigNum; i++){
+        sum+=i;
+    }
+
+    return sum; 
+    }
+    else return "ERROR"; 
+    
 };
 
 // Do not edit below this line

790f1a40f062055a2ff7c67f79e9c8197544e04a
Alon <217150097+AlonDv@users.noreply.github.com>
2026-03-24 18:53:57 -0700

    Complete 07_REVERSESTRING

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+    return input.split("").reverse().join("");
 };
+
+
+
+console.log(reverseString("hello there"));
+
 
 // Do not edit below this line
 module.exports = reverseString;

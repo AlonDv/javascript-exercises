ac651996b07e8c2e9bfc150bb9a278fd545b187b
Alon <217150097+AlonDv@users.noreply.github.com>
2026-03-25 17:24:02 -0700

    COmplete 11_TEMPCONVERSION exercise.

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fah) {
+  let celsius = ((fah - 32) * (5/9)).toFixed(1);
+  return +celsius; 
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let fah = ((celsius*(9/5))+32).toFixed(1);
+  return +fah; 
 };
 
 // Do not edit below this line
